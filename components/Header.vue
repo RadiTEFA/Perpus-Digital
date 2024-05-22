eca8105c033cecaf8bb488c9ce680e9c4e22ac79
RadiTEFA <radingoding@gmail.com>
2024-05-22 10:16:35 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="header">
+    <div class="h-img">
+      <img src="~/assets/img/LOGO-SMK4.png" alt="logo smkn 4">
+    </div>
+    <div class="t-judul">
+      <h1>Perpustakaan Digital</h1>
+      <h6>SMK Negeri 4 Tasikmalaya</h6>
+      <h6>Jl Depok Kel.Sukamenak Kec.Purbaratu</h6>
+    </div>
+  </div>
+</template>
+<style scoped>
+.header{
+  height: 150px;
+  display: flex;
+  align-items: center;
+  background-color: #0075FF;
+  color: #ffffff;
+}
+.h-img >  img {
+  width: 80px;
+  margin-left: 50px;
+}
+.t-judul{
+  font-family: 'Times New Roman', Times, serif;
+  margin-left: 40px;
+}
+</style>
---
+++ components/Statistik.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <canvas id="statistik"></canvas>
+    </div>
+</template>
+
+<script setup>
+import Chart from 'chart.js/auto'
+
+const labels = [
+    'Januari',
+    'Februari',
+    'Maret',
+    'April',
+    'Mei',
+    'Juni',
+    'Juli',
+    'Agustus',
+    'September',
+    'Oktober',
+    'November',
+    'Desember'
+  ];
+
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'Pengunjung',
+      data: [1, 5, 7, 4, 10, 6, 3, 2, 8, 9, 0, 0],
+      backgroundColor: [
+      'rgb(0, 120, 255)',
+      'rgb(0, 120, 255)',
+      'rgb(0, 120, 255)',
+      'rgb(0, 120, 255)',
+      'rgb(0, 120, 255)',
+      'rgb(0, 120, 255)',
+      'rgb(0, 120, 255)',
+      'rgb(0, 120, 255)',
+      'rgb(0, 120, 255)',
+      'rgb(0, 120, 255)',
+      'rgb(0, 120, 255)',
+      'rgb(0, 120, 255)'
+    ],
+    }]
+  };
+
+  const config = {
+    type: 'bar',
+    data: data,
+    options: {}
+  };
+
+
+onMounted(() => {
+  const myChart = new Chart(
+    document.getElementById('statistik'),
+    config
+  );
+})
+</script>
